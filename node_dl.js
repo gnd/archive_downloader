2849a625d00bd8ee7d0fca2cd7c22b88ab0dc4aa
root <root@mai>
2019-05-05 15:06:35 +0200

    small fixes

--- node_dl.js
+++ node_dl.js
@@ -7,24 +7,27 @@ Instalation:
 
 How to save a book:
 - request the book
-- login in your fav browser and start reading the book
+- login in your fav browser (make sure you are not using any proxy or VPN for that) and start reading the book
 - copy your cookies into the script
-- fill out the rest of the globals
+    - you can use the extension EditThisCookie in Chrome. In Options of the extension first set the preffered export format to be 'Semicolo separated name=value pairs'
+    - export the cookies and just copy the into this file
+- fill out the rest of the globals, eg. user agent and number of pages
 - run like: node node_dl.js
 
-
-gnd, 2018, initial impulse by db @ mnskp
+gnd, 2018-2019, initial impulse by db @ mnskp
 */
 
 /* GLOBALS */
 // the actual url of the archive.org server, get it from the html source when reading the book
-var url_stub = 'https://ia601900.us.archive.org/BookReader/BookReaderImages.php?zip=/19/items/ARes22722/ARes22722_jp2.zip&file=ARes22722_jp2/ARes22722_0'
+var url_stub = 'https://ia902307.us.archive.org/BookReader/BookReaderImages.php?zip=/0/items/isbn_9780805067811/isbn_9780805067811_jp2.zip&file=isbn_9780805067811_jp2/isbn_9780805067811_'
 // copy your cookies when reading the book, use a plugin like HTTP Headers (or EditThisCookie) for Chrome,
-var cookies = ''
+var cookies = '';
+// copy your User-Agent here
+var ua = 'Mozilla/5.0 (X11; Linux x86_64)';
 // how many pages to download
-var pages = 10
+var pages = 252
 // how should the images be called
-var local_name = 'aristophanes'
+var local_name = 'books/life_death/life_death'
 // how long to wait between requests (in ms)
 var sleep_ms = 100
 
@@ -46,25 +49,18 @@ if (cookies === "") {
 
 // start download
 for (var i = 1; i < pages + 1; i++) {
-    var padding_a = "";
-    var padding_b = "";
-    if (i < 10) {
-        padding_a = "0";
-    }
-    if (i < 100) {
-        padding_b = "0";
-    }
-    var url = url_stub + padding_a + padding_b + i + ".jp2&scale=2&rotate=0";
+    var padding = ("000" + i).slice(-4)
+    var url = url_stub + padding + ".jp2&scale=0&rotate=0";
+    //console.log("Getting: " + url);
     var options = {
         url: url,
         headers: {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Encoding': 'deflate, br',
-            'Accept-Language': 'en-US,en;q=0.9',
+            'User-Agent': ua,
             'Cookie': cookies
         }
     };
-    var filename = local_name + i + ".jp2";
+    var filename = local_name + '_' + padding + ".jp2";
     download(options, filename, function() {
         console.log('Image downloaded');
     });
